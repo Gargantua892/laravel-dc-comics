c9cdd261e87843ee37a2d33df35c0bb359c91435
gargantua892 <94129392+And18892@users.noreply.github.com>
2023-07-26 23:02:23 +0200

    ultime correzioni

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,38 +4,38 @@
 
     <div class="text-center py-2">
         <h1>CREATE NEW COMIC</h1>
-        <form method="POST" action="{{ route('show') }}" class="py-3">
+        <form method="POST" action="{{ route('update') }}" class="py-3">
 
             @csrf
             @method("POST")
 
             <label for="title">Title</label>
             <br>
-            <input type="text" name="title" id="title" value="title">
+            <input type="text" name="title" id="title" value="{{ $comic -> name }}">
             <br>
             <label for="description">Description</label>
             <br>
-            <input type="text" name="description" id="description" value="description">
+            <input type="text" name="description" id="description" value="{{ $comic -> description }}">
             <br>
             <label for="thumb">Thumb</label>
             <br>
-            <input type="text" name="thumb" id="thumb" value="thumb">
+            <input type="text" name="thumb" id="thumb" value="{{ $comic -> thumb }}">
             <br>
             <label for="price">Price</label>
             <br>
-            <input type="text" name="price" id="price" value="price">
+            <input type="text" name="price" id="price" value="{{ $comic -> price}}">
             <br>
             <label for="series">Series</label>
             <br>
-            <input type="text" name="series" id="series" value="series">
+            <input type="text" name="series" id="series" value="{{ $comic -> series}}">
             <br>
             <label for="sale_date">Sale Date</label>
             <br>
-            <input type="date" name="sale_date" id="sale_date" value="sale_date">
+            <input type="date" name="sale_date" id="sale_date" value="{{ $comic -> sale_date}}">
             <br>
             <label for="type">Type</label>
             <br>
-            <input type="text" name="type" id="type" value="type">
+            <input type="text" name="type" id="type" value="{{ $comic -> type}}">
             <br>
 
             <input class="mt-4 px-5 rounded" type="submit" value="CREATE">
